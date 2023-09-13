9b6178cf187a411db149365d16543cd821aceb55
Manu Jourdan <jourdan.manu@gmail.com>
2023-09-13 20:50:47 +0200

    Migrate html to .php file & other changes

--- PropertyPage/property.php
+++ PropertyPage/property.php
@@ -10,12 +10,17 @@
     <title>Diggit</title>
     <link rel="stylesheet" href="property.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Suez One">
 </head>
 <body>
     <div class="background-sidebar-container">
         <div class="sidebar">
             <div class="logo-container">
-                <div class="logo">DiggIt</div>
+                <a id="logo" href="../IndexPage/index.php">
+                    <div id="digg">Digg</div>
+                    <div id="it">It</div>
+                </a>
+
             </div>
             <div class="page-indicator-container">
                 <a href="#">Property</a>
@@ -33,10 +38,16 @@
             <div class="empty-div"></div>
             <div class="searchbar">
                 <div class="text-icon-container">
-                    <div class="search-icon">[]</div>
-                    <div class="search-text">Search</div>
+                    <button type="submit" class="searchButton" id="searchButton">
+                        <svg class="svgSearch" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.6705 16.5218L19.6773 16.5121L19.6837 16.5021C20.709 14.8889 21.3112 12.9735 21.3112 10.9149C21.3112 5.16412 16.6544 0.499512 10.9089 0.499512C5.15702 0.499512 0.5 5.1639 0.5 10.9149C0.5 16.6656 5.15681 21.3302 10.9023 21.3302C12.9878 21.3302 14.9306 20.7146 16.5581 19.6615L16.5651 19.6569L16.572 19.6522L16.6779 19.5785L23.4524 26.3531L23.8091 26.7098L24.1626 26.3499L26.3567 24.1169L26.7038 23.7635L26.3537 23.413L19.5871 16.6402L19.6705 16.5218ZM16.1022 5.72806C17.4862 7.1121 18.2474 8.95104 18.2474 10.9084C18.2474 12.8657 17.4862 14.7046 16.1022 16.0887C14.7181 17.4727 12.8792 18.2339 10.9219 18.2339C8.96454 18.2339 7.1256 17.4727 5.74157 16.0887C4.35754 14.7046 3.59635 12.8657 3.59635 10.9084C3.59635 8.95104 4.35754 7.1121 5.74157 5.72806C7.1256 4.34403 8.96455 3.58284 10.9219 3.58284C12.8792 3.58284 14.7181 4.34403 16.1022 5.72806Z" fill="#AD5511" stroke="#AD5511" />
+                        </svg>
+                    </button>
+                    <input id="searchbar" type="text" class="searchTerm" spellcheck="false" placeholder="Find your Digs..">
                 </div>
-                <div class="crab-logo">crab</div>
+                <div id="dropdown" class="dropdown-content">
+                    <div class="crab-logo">crab</div>
+                </div>
             </div>
             <div class="profile-container">
                 <button class="profile">profile</button>
@@ -96,7 +107,7 @@
                                     <input type="text">
                                     <input type="text">
                                     <input type="text">
-                                    <label for="message">Message</label>
+                                    <label id="message-label" for="message">Message</label>
                                     <textarea name="message" id="message" rows="12"></textarea>
                                 </form>
                             </div>
