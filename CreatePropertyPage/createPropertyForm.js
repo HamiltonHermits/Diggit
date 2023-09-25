62ccdfd5e4d750974a2946207a7f6c4d9fe12c96
masterdoodles <masterdoodlestyler@gmail.com>
2023-09-25 23:44:51 +0200

    Worked On createProperty

---
+++ CreatePropertyPage/createPropertyForm.js
@@ -0,0 +1,25 @@
+//firstly im going to grab each element that the form needs
+
+//property title input
+var newPropertyTitleInput = document.getElementById('newPropertyTitle');
+
+//addimage button 
+var addImageButton = document.getElementById('addImage');
+//the actual location of the image 
+var imageFile = document.getElementById('file');
+
+//im not sure how this works but for now im just gonna keep map here
+var map = document.getElementById('map');
+
+//I need to get the values of every checklist and see if it is set or not. 
+var wifi = document.getElementById('wifi');
+var parking = document.getElementById('parking');
+var waterTanks = document.getElementById('waterTanks');
+var laundry = document.getElementById('laundry');
+var stove = document.getElementById('stove');
+var fridge = document.getElementById('fridge');
+var microwave = document.getElementById('microwave');
+
+//now i need to grab the list of Tenants from the html
+
+
