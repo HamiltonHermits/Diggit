5f502605c38af2b21422ba0ab2583fdd2f93340d
Wynne Edwards <g21e2079@campus.ru.ac.za>
2023-09-30 12:20:22 +0200

    config

--- Backend_Files/config.php
+++ Backend_Files/config.php
@@ -4,9 +4,9 @@ $db_host = "IS3-DEV.ICT.RU.AC.ZA";
 $db_user = "HamiltonHermits";
 $db_pass = "HamiltonHermits";
 $db_name = "hamiltonhermits";
-?>
-
 // $db_host = "localhost";
 // $db_user = "root";
 // $db_pass = "";
 // $db_name = "hamiltonhermits";
+?>
+
