a3048533ba4dd1fb0c207195056431c5992b7189
Cameron <g20w0388@campus.ru.ac.za>
2023-10-01 15:28:06 +0200

    fixedcomments

--- PropertyPage/comments.php
+++ PropertyPage/comments.php
@@ -1,22 +1,42 @@
 <?php
 include('../Backend_Files/database_connect.php');
 // Define your SQL query based on the sorting option
+$propId = $_GET["id"];
 $sort_option = isset($_GET['sort-comments']) ? $_GET['sort-comments'] : 'desc';
 
 switch ($sort_option) {
     case 'asc':
-        $sql = "SELECT * FROM review,usertbl WHERE usertbl.user_id=review.user_id ORDER BY avg_prop_review ASC";
+        $sql = "SELECT *
+        FROM hamiltonhermits.review
+        JOIN hamiltonhermits.usertbl ON usertbl.user_id = review.user_id
+        JOIN hamiltonhermits.property ON property.prop_id = review.prop_id
+        WHERE property.prop_id=$propId
+        ORDER BY overall_property_rating ASC";
         break;
     case 'oldest':
-        $sql = "SELECT * FROM review,usertbl WHERE usertbl.user_id=review.user_id ORDER BY avg_prop_review ASC";
+        $sql = "SELECT *
+        FROM hamiltonhermits.review
+        JOIN hamiltonhermits.usertbl ON usertbl.user_id = review.user_id
+        JOIN hamiltonhermits.property ON property.prop_id = review.prop_id
+        WHERE property.prop_id=$propId
+        ORDER BY date_reviewed ASC";
         break;
     case 'newest':
-        $sql = "SELECT * FROM review,usertbl WHERE usertbl.user_id=review.user_id ORDER BY avg_prop_review DESC";
+        $sql = "SELECT *
+        FROM hamiltonhermits.review
+        JOIN hamiltonhermits.usertbl ON usertbl.user_id = review.user_id
+        JOIN hamiltonhermits.property ON property.prop_id = review.prop_id
+        WHERE property.prop_id=$propId ORDER BY date_reviewed DESC";
         break;
     case 'desc':
     default:
-        $sql = "SELECT * FROM review,usertbl WHERE usertbl.user_id=review.user_id ORDER BY avg_prop_review DESC";
-        break;
+         $sql = "SELECT *
+         FROM hamiltonhermits.review
+         JOIN hamiltonhermits.usertbl ON usertbl.user_id = review.user_id
+         JOIN hamiltonhermits.property ON property.prop_id = review.prop_id
+         WHERE property.prop_id=$propId
+         ORDER BY overall_property_rating DESC";
+           break;
 }
 
 // Execute the SQL query and fetch comments
@@ -29,7 +49,7 @@ if (!$result) {
 // Display comments
 while ($row = mysqli_fetch_assoc($result)) {
     echo '<div class ="comment-container">';
-    echo '<div class = "star-rating-comment">' . $row['avg_prop_review'] . '</div>';
+    echo '<div class = "star-rating-comment">' . $row['overall_property_rating'] . '</div>';
     echo '<div class = "username-date-comment">' . $row['username'] . ' - ' . $row['date_reviewed'] . '</div>';
     echo '<div class = "description-comment">' . $row['written_review'] . '</div>';
     echo '<hr class = "horizontal-line-comment">';
