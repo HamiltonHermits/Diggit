b2b4943229833728f8433742ade7c252117df00f
masterdoodles <masterdoodlestyler@gmail.com>
2023-10-01 16:11:58 +0200

    agent slider

--- PropertyPage/process_ratings.php
+++ PropertyPage/process_ratings.php
@@ -5,23 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Initialize a response array
     $response = array();
 
-    // Get the data sent via POST
-    $propertyId = isset($_POST['propertyId']) ? $_POST['propertyId'] : '';
-    $userId = isset($_POST['userId']) ? $_POST['userId'] : '';
-    $propertyReview = isset($_POST['property_review']) ? $_POST['property_review'] : '';
-    //star ratings
-    $cleanliness = isset($_POST['cleanliness']) ? $_POST['cleanliness'] : '';
-    $noise = isset($_POST['noise']) ? $_POST['noise'] : '';
-    $location = isset($_POST['location']) ? $_POST['location'] : '';
-    $safety = isset($_POST['safety']) ? $_POST['safety'] : '';
-    $affordability = isset($_POST['affordability']) ? $_POST['affordability'] : '';
-    $overallRating = isset($_POST['overallRating']) ? $_POST['overallRating'] : '';
-     //slider rating
-    $politenessRating = isset($_POST['politenessRating']) ? $_POST['politenessRating'] : '';
-    $repairRating = isset($_POST['repairRating']) ? $_POST['repairRating'] : '';
-    $responseTimeRating = isset($_POST['responseTimeRating']) ? $_POST['responseTimeRating'] : '';
-    $overallLandlordRating = isset($_POST['overallLandlordRating']) ? $_POST['overallLandlordRating'] : '';
-
     $requiredFields = [
         'propertyId',
         'userId',
@@ -37,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'responseTimeRating',
         'overallLandlordRating',
     ];
-    
+
     $allFieldsSet = true;
-    
+
     foreach ($requiredFields as $field) {
         if (!isset($_POST[$field]) || empty($_POST[$field])) {
             $allFieldsSet = false;
@@ -47,37 +30,66 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
     if ($allFieldsSet) {
-        // All required fields are set and not empty
-        // You can proceed with processing the data here
-        // Example: $propertyId = $_POST['propertyId'];
+        // Get the data sent via POST
+        $propertyId = isset($_POST['propertyId']) ? $_POST['propertyId'] : '';
+        $userId = isset($_POST['userId']) ? $_POST['userId'] : '';
+        $propertyReview = isset($_POST['property_review']) ? $_POST['property_review'] : '';
+        //star ratings
+        $cleanliness = isset($_POST['cleanliness']) ? $_POST['cleanliness'] : '';
+        $noise = isset($_POST['noise']) ? $_POST['noise'] : '';
+        $location = isset($_POST['location']) ? $_POST['location'] : '';
+        $safety = isset($_POST['safety']) ? $_POST['safety'] : '';
+        $affordability = isset($_POST['affordability']) ? $_POST['affordability'] : '';
+        $overallRating = isset($_POST['overallRating']) ? $_POST['overallRating'] : '';
+        //slider rating
+        $politenessRating = isset($_POST['politenessRating']) ? $_POST['politenessRating'] : '';
+        $repairRating = isset($_POST['repairRating']) ? $_POST['repairRating'] : '';
+        $responseTimeRating = isset($_POST['responseTimeRating']) ? $_POST['responseTimeRating'] : '';
+        $overallLandlordRating = isset($_POST['overallLandlordRating']) ? $_POST['overallLandlordRating'] : '';
     } else {
         // Handle the case where not all required fields are set
-        echo "Not all required fields are set.";
+        $response['success'] = false;
+        $response['message'] = 'this ran $propertyId successfully' . $something;
+        echo json_encode($response);
     }
 
     // Handle the star ratings and slider ratings
     // Convert the response array to a single string
     // if(!isset($propertyId)&&) ;
-    
-    // try{//sql query for adding a review
-    //     $query = "INSERT INTO review (`prop_id`, `politeness_rating`, `written_review`, `cleanliness_rating`,
-    //      `noise_rating`, `location_rating`, `saftey_rating`, `affordability_rating`, `repair_quality_rating`, 
-    //      `response_time_rating`, `user_id`, `avg_prop_review`, `overall_property_rating`)
-    //     VALUES ('$propertyId','$starRatings[$category]')
-    //     ";
-    //     $result = mysqli_query($conn, $query);
-    //     $row = mysqli_fetch_assoc($result);
-    //     $something = $row['prop_id'];
 
-    // }catch(Exception $e){
-    //     http_response_code(400); // Bad Request
-    //     echo json_encode(["error" => $e->getMessage()]);
+    try{//sql query for adding a review
+        $query = "INSERT INTO review (
+            `prop_id`, 
+            `politeness_rating`,
+            `written_review`,
+            `cleanliness_rating`,
+            `noise_rating`,
+            `location_rating`, 
+            `saftey_rating`, 
+            `affordability_rating`, 
+            `repair_quality_rating`, 
+            `response_time_rating`, 
+            `user_id`, 
+            `avg_prop_review`, 
+            `overall_property_rating`)
+        VALUES (
+            '$propertyId',
+            '$starRatings[$category]'
+        )
+        ";
+        $result = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($result);
+        $something = $row['prop_id'];
 
-    // }
+    }catch(Exception $e){
+        http_response_code(400); // Bad Request
+        echo json_encode(["error" => $e->getMessage()]);
+
+    }
 
     // For demonstration purposes, we'll just create a response array
     $response['success'] = true;
-    $response['message'] = 'this ran $propertyId successfully'.$something;
+    $response['message'] = 'this ran $propertyId successfully' . $something;
 
     // Send the JSON response
     header('Content-Type: application/json');
