c57ffe59be2960f3b46aaaf237bf9d4d4ffe5358
Manu <jourdan.manu@gmail.com>
2023-10-02 16:16:13 +0200

    ???

--- ReportPage/chartReports.php
+++ ReportPage/chartReports.php
@@ -1,19 +1,15 @@
-<!-- report.php -->
 <?php
 require_once('../Backend_Files/database_connect.php');
 
-// Get the criteria and search
-$criteriaTerm = isset($_GET['criteria']) ? $_GET['criteria'] : '';
-$searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
+// Get the chart type and search
+$chartType = isset($_GET['chart']) ? $_GET['chart'] : '';
 
-switch ($criteriaTerm) {
-    case 'cleanliness':
-        $selectedCriteria = 'cleanliness_rating';
-        $reviewSelectedCriteria = 'review.' . $selectedCriteria;
-        $query = "SELECT property.prop_name, $reviewSelectedCriteria
+switch ($chartType) {
+    case 'overall-prop-rating-cc':
+        $query = "SELECT property.prop_name, review.overall_property_rating
                   FROM property
                   JOIN review ON property.prop_id = review.prop_id
-                  WHERE property.prop_name LIKE '%" . $searchTerm . "%'";
+                  ORDER BY review.overall_property_rating DESC";
         break;
     case 'noise':
         $selectedCriteria = 'noise_rating';
@@ -55,24 +51,14 @@ switch ($criteriaTerm) {
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-    // Output the table header with column names
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Property Name</th>";
-    echo "<th>Criteria Rating</th>";
-    echo "</tr>";
-
     while ($row = $result->fetch_assoc()) {
-        // Generate HTML rows for the table
-        echo "<tr>";
-        echo "<td>" . $row['prop_name'] . "</td>";
-        echo "<td>" . $row[$selectedCriteria] . "</td>";
-        echo "</tr>";
+        $response[] = $row; // Add each row to the response array
     }
-    echo "</table>";
+    echo json_encode($response); 
 } else {
     echo "<tr><td colspan='7'>No data found</td></tr>";
 }
+
 
 $conn->close();
 ?>
--- ReportPage/dashboard.php
+++ ReportPage/dashboard.php
@@ -20,6 +20,7 @@
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script src="dashboard.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 
 <body>
@@ -157,10 +158,10 @@
                 <div class="left-box">
                     <div class="report-title-container">
                         <div class="report-title">
-                            Tenant Report
+                            Chart Reports
                         </div>
                     </div>
-                    <div class="searchbar-container-container">
+                    <!-- <div class="searchbar-container-container">
                         <div class="searchbar-container">
                             <div class="borderSearchBar" id="borderSearchBar">
                                 <button type="submit" class="searchButton" id="searchButton">
@@ -172,15 +173,25 @@
                                 <div id="dropdown" class="dropdown-content"></div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div id="generateReportButton" class="generateReportButton">
-                        <button id="generateTenantReport" class="filledButton">Generate Report</button>
+                        <button id="generateChartReport" class="filledButton">Generate Report</button>
+                        <button id="chooseChart" class="filledButton" name="chooseChart">
+                            <div id="chart-select-placeholder">Choose Chart</div> 
+                            <select id="chart-dropdown" name="chart-dropdown">
+                            <option value="null"></option>
+                                <option value="overall-prop-rating-cc">Overall Property Rating - Column chart</option>
+                                <!-- <option value="noise">Noise</option>
+                                <option value="location">Location</option>
+                                <option value="safety">Safety</option>
+                                <option value="affordability">Affordability</option> -->
+                            </select>
+                        </button>
                     </div>
-                    <div class="report-container" id="tenant-report-container">
-                        <div class="report-text-container" id="tenant-report-text-container">
+                    <div class="report-container" id="chart-report-container">
+                        <div class="chart-text-container" id="chart-report-text-container">
                             <!-- php here -->
                         </div>
-
                     </div>
                 </div>
 
@@ -198,7 +209,7 @@
                                         <path d="M16.6 18.5L10.3 12.2C9.8 12.6 9.225 12.9167 8.575 13.15C7.925 13.3833 7.23333 13.5 6.5 13.5C4.68333 13.5 3.14583 12.8708 1.8875 11.6125C0.629167 10.3542 0 8.81667 0 7C0 5.18333 0.629167 3.64583 1.8875 2.3875C3.14583 1.12917 4.68333 0.5 6.5 0.5C8.31667 0.5 9.85417 1.12917 11.1125 2.3875C12.3708 3.64583 13 5.18333 13 7C13 7.73333 12.8833 8.425 12.65 9.075C12.4167 9.725 12.1 10.3 11.7 10.8L18 17.1L16.6 18.5ZM6.5 11.5C7.75 11.5 8.8125 11.0625 9.6875 10.1875C10.5625 9.3125 11 8.25 11 7C11 5.75 10.5625 4.6875 9.6875 3.8125C8.8125 2.9375 7.75 2.5 6.5 2.5C5.25 2.5 4.1875 2.9375 3.3125 3.8125C2.4375 4.6875 2 5.75 2 7C2 8.25 2.4375 9.3125 3.3125 10.1875C4.1875 11.0625 5.25 11.5 6.5 11.5Z" fill="#D9D9D9" />
                                     </svg>
                                 </button>
-                                <input id="searchbar" type="text" class="searchTerm" spellcheck="false" placeholder="Search for a Property..">
+                                <input id="criteria-searchbar" type="text" class="searchTerm" spellcheck="false" placeholder="Search for a Property..">
                                 <div id="dropdown" class="dropdown-content"></div>
                             </div>
                         </div>
@@ -208,6 +219,7 @@
                         <button id="chooseCriteria" class="filledButton" name="chooseCriteria">
                             <div id="select-placeholder">Choose Criteria</div> 
                             <select id="criteria-dropdown" name="criteria-dropdown">
+                                <option value="null"></option>
                                 <option value="cleanliness">Cleanliness</option>
                                 <option value="noise">Noise</option>
                                 <option value="location">Location</option>
