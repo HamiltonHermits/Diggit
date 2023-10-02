fc31a60883a631beff96da9bc8058653d55eea17
Cameron <g20w0388@campus.ru.ac.za>
2023-10-02 11:45:30 +0200

    added deleteph code, need to update searchbar

--- PropertyPage/delete.php
+++ PropertyPage/delete.php
@@ -1,15 +1,16 @@
 <?php
+/////////////////////////////////////////////////////////////////
+
+// NEED TO UPDATE SEARCH BAR TO CHECK FOR IS DELETED != 1 //////
+
+
+////////////////////////////////////////////////////////////////
 include('../Backend_Files/database_connect.php');
 // Define your SQL query based on the sorting option
 $propId = $_GET["id"];
 $sort_option = isset($_GET['sort']) ? $_GET['sort'] : 'desc';
 
-$sql = "SELECT *
-        FROM hamiltonhermits.review
-        JOIN hamiltonhermits.usertbl ON usertbl.user_id = review.user_id
-        JOIN hamiltonhermits.property ON property.prop_id = review.prop_id
-        WHERE property.prop_id=$propId
-        ORDER BY overall_property_rating ASC";
+$sql = "UPDATE property SET is_deleted=1 WHERE prop_id=$propId";
         
 
 // Execute the SQL query and fetch comments
@@ -23,4 +24,5 @@ if (!$result) {
 
 // Close the database connection
 mysqli_close($conn);
+
 ?>
