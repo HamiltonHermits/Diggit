d32873e25aaa766f1b9350faba4766c833efb049
Cameron Wicks <cameronwicks4251@gmail.com>
2023-10-03 18:41:13 +0200

    IT WORKS

--- PropertyPage/deleteComment.php
+++ PropertyPage/deleteComment.php
@@ -3,13 +3,27 @@ include('../Backend_Files/database_connect.php');
 // Define your SQL query based on the sorting option
 $propId = $_GET["id"];
 
+$sqlUser=$sql = "SELECT *
+FROM hamiltonhermits.usertbl;";
 
+$result = mysqli_query($conn, $sqlUser);
+
+
+while ($row = mysqli_fetch_assoc($result)) {
+    if($row['username']==$_SESSION['username']){
+        $userId=$row['user_id'];
+    }
+}
+
+if (!$result) {
+    die("Database query failed: " . mysqli_error($conn));
+}
 
 
         $sql = "DELETE 
         FROM hamiltonhermits.review
         
-        WHERE review.prop_id=$propId;";
+        WHERE review.prop_id=$propId AND review.user_id=$userId;";
         
 
 // Execute the SQL query and fetch comments
