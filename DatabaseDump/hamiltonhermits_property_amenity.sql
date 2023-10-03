9ec1d7946b47454f234e507167fcaefc5aa52a52
masterdoodles <masterdoodlestyler@gmail.com>
2023-10-03 18:22:05 +0200

    database dump

--- DatabaseDump/hamiltonhermits_property_amenity.sql
+++ DatabaseDump/hamiltonhermits_property_amenity.sql
@@ -28,8 +28,9 @@ CREATE TABLE `property_amenity` (
   `amenity_id` int NOT NULL,
   PRIMARY KEY (`prop_amenity_id`),
   KEY `propId_idx` (`prop_id`),
-  KEY `amenityId_idx` (`amenity_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=185 DEFAULT CHARSET=utf8 COMMENT='Junction table';
+  KEY `amenityId_idx` (`amenity_id`),
+  CONSTRAINT `amenity_id` FOREIGN KEY (`amenity_id`) REFERENCES `amenity_test` (`amenity_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=192 DEFAULT CHARSET=utf8 COMMENT='Junction table';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +39,7 @@ CREATE TABLE `property_amenity` (
 
 LOCK TABLES `property_amenity` WRITE;
 /*!40000 ALTER TABLE `property_amenity` DISABLE KEYS */;
-INSERT INTO `property_amenity` VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(12,1,6),(13,1,7),(15,2,1),(16,11,1),(17,11,2),(18,11,1),(19,11,2),(20,13,1),(21,13,2),(22,13,3),(23,14,5),(24,14,6),(25,14,7),(26,14,5),(27,14,6),(28,14,7),(29,14,5),(30,14,6),(31,14,7),(32,17,1),(33,17,5),(34,17,7),(35,17,1),(36,17,5),(37,17,7),(38,20,1),(39,20,2),(40,20,3),(41,20,4),(42,22,2),(43,22,3),(44,22,4),(45,25,5),(46,25,6),(47,25,7),(48,26,2),(49,26,3),(50,26,4),(51,26,5),(52,26,6),(53,26,7),(54,26,8),(55,27,1),(56,27,2),(57,28,1),(58,29,1),(59,29,2),(60,30,1),(61,30,2),(62,30,3),(63,30,4),(72,32,1),(73,32,10),(74,32,12),(75,34,4),(76,34,5),(77,34,6),(78,34,7),(79,34,8),(80,35,1),(81,35,2),(82,35,3),(83,35,4),(84,35,5),(85,35,6),(86,35,7),(87,35,8),(88,35,9),(89,35,10),(90,35,11),(91,35,12),(173,31,1),(174,31,2),(175,31,3),(176,31,4),(177,31,5),(178,31,6),(179,31,7),(180,31,8),(181,31,9),(182,31,10),(183,31,11),(184,31,12);
+INSERT INTO `property_amenity` VALUES (185,36,1),(186,36,2),(187,36,5),(188,36,6),(189,36,7),(190,36,8),(191,36,10);
 /*!40000 ALTER TABLE `property_amenity` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-02 22:51:34
+-- Dump completed on 2023-10-03 18:21:29
