697fd10a7dd3994b13f131bbbcdb0fdf3bf06afd
Wynne Edwards <103907123+wynne-edwards@users.noreply.github.com>
2023-10-11 12:22:15 +0200

    reverted live server changes

--- Backend_Files/config.php
+++ Backend_Files/config.php
@@ -1,15 +1,11 @@
 <?php
-//  $db_host = "IS3-DEV.ICT.RU.AC.ZA";
-//  $db_user = "HamiltonHermits";
-//  $db_pass = "HamiltonHermits";
-//  $db_name = "hamiltonhermits";
+ $db_host = "IS3-DEV.ICT.RU.AC.ZA";
+ $db_user = "HamiltonHermits";
+ $db_pass = "HamiltonHermits";
+ $db_name = "hamiltonhermits";
 // $db_host = "localhost";
 // $db_user = "root";
 // $db_pass = "";
 // $db_name = "hamiltonhermits";
- $db_host = "DB_HOST";
- $db_user = "DB_USERNAME";
- $db_pass = "DB_PASSWORD";
- $db_name = "DB_DATABASE";
 ?>
 
