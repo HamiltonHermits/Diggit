409785d7921fe09d28b4247c4f5f703fe3462e52
Manu <jourdan.manu@gmail.com>
2023-10-11 12:54:44 +0200

    "why would it break" ~ tyler baxter 2023

--- Backend_Files/search.php
+++ Backend_Files/search.php
@@ -9,15 +9,27 @@ $filterOption = $_GET["filterOption"];
 
 switch ($filterOption) {
     case 'overallRating':
-        $stmt = $conn->prepare("SELECT DISTINCT property.prop_id, property.prop_name, 
-                                       property.prop_description, property.address,
-                                       review.overall_property_rating
-                                FROM property
-                                JOIN review ON property.prop_id = review.prop_id
-                                WHERE (property.prop_name LIKE ? 
-                                OR address LIKE ?)
-                                AND property.is_deleted = false
-                                ORDER BY review.overall_property_rating DESC");
+        $stmt = $conn->prepare("SELECT
+        property.prop_id,
+        property.prop_name,
+        property.prop_description,
+        property.address,
+        MAX(review.overall_property_rating) AS overall_property_rating
+    FROM
+        property
+    JOIN
+        review ON property.prop_id = review.prop_id
+    WHERE
+        (property.prop_name LIKE ? OR address LIKE ?)
+        AND property.is_deleted = false
+    GROUP BY
+        property.prop_id,
+        property.prop_name,
+        property.prop_description,
+        property.address
+    ORDER BY
+        overall_property_rating DESC
+    ");
         break;
     
     default:
